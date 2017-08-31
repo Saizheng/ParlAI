6e20a4161fc21e3f2c9a4ce80f8ece3258795f20
Saizheng Zhang <saizheng@fb.com>
2017-08-31 13:26:47 -0400

    fix type in mturk_manager

--- parlai/mturk/core/mturk_manager.py
+++ parlai/mturk/core/mturk_manager.py
@@ -361,7 +361,7 @@ class MTurkManager():
             # TODO kill onboarding world's thread
         elif status == AssignState.STATUS_WAITING:
             # agent is in pool, remove from pool and delete
-            agent.persona_generator.push_persona(gent.persona_dix)
+            agent.persona_generator.push_persona(agent.persona_idx)
             agent.persona_generator.save_idx_stack()
             print ("\n******* Push persona {} back to stack. *******\n".format(agent.persona_idx))
             #print(agent.persona_generator.idx_stack) 
